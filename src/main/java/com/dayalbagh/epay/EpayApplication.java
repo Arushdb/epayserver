9df9baf582205285392665a4f2d0dbc2aaedf43f
Arush Kumar <arushkumar023@gmail.com>
2023-01-15 18:29:30 +0530

    war file changes

--- src/main/java/com/dayalbagh/epay/EpayApplication.java
+++ src/main/java/com/dayalbagh/epay/EpayApplication.java
@@ -2,12 +2,24 @@ package com.dayalbagh.epay;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EpayApplication {
+public class EpayApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EpayApplication.class, args);
 	}
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		
+		return builder.sources(EpayApplication.class);
+	
+	}
+	
 
 }
